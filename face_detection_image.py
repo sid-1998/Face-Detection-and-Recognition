c983d93b2482fdf8d3464e6a14cb540c5d7132fb
Siddharth Pahuja <sid.pahuja2008@gmail.com>
2019-01-30 05:30:03 +0530

    testing
    Signed-off-by: Siddharth Pahuja <sid.pahuja2008@gmail.com>

--- face_detection_image.py
+++ face_detection_image.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-path = './test_images/dp.png'
+path = './test_images/index.jpg'
 img = cv2.imread(path)
 
 face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_alt.xml")
@@ -10,4 +10,6 @@ faces = face_cascade.detectMultiScale(img,1.3,5)
 for face in faces:
     x,y,w,h = face
     cv2.rectangle(img, (x,y), (x+w, y+h), (0,0,255), 2)
-    img_detect = cv2.imwrite('./output/' + "img.jpg", img)
+    cv2.imwrite('./output/' + "img.jpg", img)
+    print("Done")
+    
